1be416c6471eda73ed14f01b7f4b5fbb6e8523a4
Fizioh <batrom@laposte.net>
2021-05-22 22:19:35 +0200

    create function to return a quote

--- san_antonio.py
+++ san_antonio.py
@@ -13,3 +13,22 @@ characters = [
     "le chat potté", 
     "Kirikou"
 ]
+
+user_answer = input("Tapez entrée pour connaitre une autre citation ou B pour quitter")
+
+if user_answer == "B":
+    pass
+elif user_answer == "C":
+    print("Ce n'est pas la bonne réponse...")
+else:
+    pass
+
+
+def show_random_quote(my_list):
+    item = my_list[1]
+    print(item)
+    return "Le programme est terminé"
+
+print(show_random_quote(quotes))
+
+    
