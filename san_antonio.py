6e1cdc17ac7f9a02428454066569b3ae1a79c7e0
Fizioh <batrom@laposte.net>
2021-05-24 16:21:32 +0200

    import module random and insert it into item

--- san_antonio.py
+++ san_antonio.py
@@ -1,9 +1,10 @@
 # -*- coding: utf8 -*-
+import random
+
 quotes = [
     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
     "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
 ]
-
 characters = [
     "alvin et les Chipmunks", 
     "Babar", 
@@ -13,22 +14,22 @@ characters = [
     "le chat potté", 
     "Kirikou"
 ]
-
+def get_random_quote(my_list):
+    rand_numb = random.randint(0, len(my_list) -1)
+    item = my_list[rand_numb]
+    return item
 user_answer = input("Tapez entrée pour connaitre une autre citation ou B pour quitter")
 
-if user_answer == "B":
-    pass
-elif user_answer == "C":
-    print("Ce n'est pas la bonne réponse...")
-else:
-    pass
 
 
-def show_random_quote(my_list):
-    item = my_list[1]
-    print(item)
-    return "Le programme est terminé"
+while user_answer != "B":
+    print(get_random_quote(quotes))
+    user_answer = input("Tapez entrée pour connaitre une autre citation ou B pour quitter")
+    
+for character in characters:
+    name_character = character.capitalize()
+    print (name_character)
 
-print(show_random_quote(quotes))
+print(get_random_quote(quotes))
 
     
