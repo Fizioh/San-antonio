6a8f28d5280901909c3d57f7532e8270791bac30
Fizioh <batrom@laposte.net>
2021-05-24 18:35:27 +0200

    create json and convert it in a Python object

--- san_antonio.py
+++ san_antonio.py
@@ -1,35 +1,46 @@
 # -*- coding: utf8 -*-
 import random
+import json
 
 quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+    "Nakama desho ne ?", 
+    "Ikuze mina !",
+    "Kaizoku ore wa naru",
+    "Oden ni soro"
 ]
-characters = [
-    "alvin et les Chipmunks", 
-    "Babar", 
-    "betty boop", 
-    "calimero", 
-    "casper", 
-    "le chat potté", 
-    "Kirikou"
-]
-def get_random_quote(my_list):
+
+
+def read_value_from_json():
+    values = []
+    
+    with open('characters.json') as f:
+        data = json.load(f)
+    for entry in data:
+        values.append(entry['character'])
+    return values
+    
+def get_random_item(my_list):
     rand_numb = random.randint(0, len(my_list) -1)
     item = my_list[rand_numb]
     return item
+
+def random_character():
+    all_values = read_value_from_json()
+    return get_random_item(all_values)
+
+def capitalize(words):
+    for word in words:
+        word.capitalize()
+        
+def message(character, quote):
+    capitalize(character)
+    capitalize(quote)
+    return "{} a dit : {}".format(character, quote)
+
 user_answer = input("Tapez entrée pour connaitre une autre citation ou B pour quitter")
 
-
-
 while user_answer != "B":
-    print(get_random_quote(quotes))
+    print(message(random_character(), get_random_item(quotes)))
     user_answer = input("Tapez entrée pour connaitre une autre citation ou B pour quitter")
-    
-for character in characters:
-    name_character = character.capitalize()
-    print (name_character)
-
-print(get_random_quote(quotes))
 
     
